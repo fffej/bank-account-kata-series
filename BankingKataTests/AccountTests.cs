1069730336bc3bd5d8156f8edbdce91716124ecd
Jeff Foster <jeff.foster@acm.org>
2015-05-29 09:59:47 +0100

    Deposit a pound and verify I get a receipt

---
+++ BankingKataTests/AccountTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BankingKataTests
+{
+    public class AccountTests
+    {
+        [Fact]
+        public void Accounts_Can_Make_Deposits()
+        {
+            var account = new Account();
+            var receipt = account.Deposit(Money.Pound);
+
+            Assert.NotNull(receipt);
+        }
+
+        public enum Money
+        {
+            Pound
+        }
+
+        public class Account
+        {
+            public object Deposit(object money)
+            {
+                return new object();
+            }
+        }
+    }
+}
